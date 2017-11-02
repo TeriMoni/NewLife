a53d256203fc6ff83613723fb4bd15b90db06016
TeriMoni <351111617@qq.com>
2017-11-02 17:21:31 +0800

    登录拦截

--- routers/router.go
+++ routers/router.go
@@ -2,9 +2,35 @@ package routers
 
 import (
 	"NewLife/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.ListArticleController{})
+	beego.Router("/404.html", &controllers.BaseController{}, "*:Go404")
+
+	beego.Router("/article", &controllers.ListArticleController{})
+	beego.Router("/article/:id", &controllers.ShowArticleController{})
+
+	beego.Router("/login", &controllers.LoginUserController{})
+	beego.Router("/logout", &controllers.LogoutUserController{})
+
+	beego.Router("/article/add", &controllers.AddArticleController{})
+	beego.Router("/article/edit/:id", &controllers.EditArticleController{})
+
+	beego.Router("/comment/add", &controllers.AddCommentController{})
+	beego.Router("/comment/edit/status", &controllers.EditCommentController{})
+
+	beego.Router("/album", &controllers.ListAlbumController{})
+	beego.Router("/album/upload", &controllers.UploadAlbumController{})
+	beego.Router("/album/edit", &controllers.EditAlbumController{})
+
+	beego.Router("/about", &controllers.AboutUserController{})
+
+	beego.Router("/uploadmulti", &controllers.UploadMultiController{})
+	beego.Router("/upload", &controllers.UploadController{})
+
+	//beego.Router("/article/ajax/add", &controllers.AddArticleController{}, "*:AddPost")
+	//beego.Router("/article/add", &controllers.AddArticleController{}, "*:Add")
 }
