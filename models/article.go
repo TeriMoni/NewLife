0c7488cafeede27ca2184ebc91919e531cffbe3c
TeriMoni <351111617@qq.com>
2017-11-13 17:27:16 +0800

    新增分类信息orm

--- models/article.go
+++ models/article.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"time"
 
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
@@ -15,9 +14,9 @@ type Article struct {
 	Keywords string
 	Summary  string
 	Content  string
-	Category *Category `orm:"rel(one)"`
+	Category *Category `json:"category" orm:"rel(fk)"`
 	Author   string
-	Created  int64
+	Created  string
 	Viewnum  int
 	Status   int
 	Tag string
@@ -33,11 +32,13 @@ type Category struct {
 	Description    string
 	Author    string
 	Status   int
+	Articles []*Article `orm:"reverse(many)"`
 }
 
 func (this *Article) TableName() string {
 	return "article"
 }
+
 
 func init() {
 	//orm.RegisterDriver("mysql", orm.DRMySQL)
@@ -48,9 +49,9 @@ func init() {
 
 func GetArticle(id int) (Article, error) {
 	o := orm.NewOrm()
-	o.Using("default")
-	art := Article{Id: id}
-	err := o.Read(&art)
+	qs := o.QueryTable("article").OrderBy("-created")
+	var art Article
+	err := qs.Filter("Id",id).RelatedSel().One(&art)
 
 	//if err == orm.ErrNoRows {
 	//return art, nil
@@ -64,7 +65,6 @@ func UpdateArticle(id int, updArt Article) error {
 	art := Article{Id: id}
 
 	art.Title = updArt.Title
-	art.Uri = updArt.Uri
 	art.Keywords = updArt.Keywords
 	art.Summary = updArt.Summary
 	art.Content = updArt.Content
@@ -74,6 +74,7 @@ func UpdateArticle(id int, updArt Article) error {
 	art.Visibility = updArt.Visibility
 	art.Titlepic = updArt.Titlepic
 	art.Category = updArt.Category
+	art.Created = updArt.Created
 	_, err := o.Update(&art)
 	return err
 }
@@ -89,19 +90,20 @@ func AddArticle(updArt Article) (int64, error) {
 	art.Summary = updArt.Summary
 	art.Content = updArt.Content
 	art.Author = updArt.Author
-	art.Created = time.Now().Unix()
+	art.Created = updArt.Created
 	art.Viewnum = 1
 	art.Status = updArt.Status
 	art.Tag = updArt.Tag
 	art.Visibility = updArt.Visibility
 	art.Titlepic = updArt.Titlepic
+	art.Category = updArt.Category
 	id, err := o.Insert(art)
 	return id, err
 }
 
 func ListArticle(condArr map[string]string, page int, offset int) (num int64, err error, art []Article) {
 	o := orm.NewOrm()
-	qs := o.QueryTable("article")
+	qs := o.QueryTable("article").OrderBy("-created")
 	cond := orm.NewCondition()
 	if condArr["title"] != "" {
 		cond = cond.And("title__icontains", condArr["title"])
@@ -143,19 +145,66 @@ func CountArticle(condArr map[string]string) int64 {
 	return num
 }
 
-func GetAllCategory()(category []Category){
+func GetAllCategory()(category []*Category){
 	o := orm.NewOrm()
-	var categorys []Category
+	var categorys []*Category
 	qs := o.QueryTable("category")
-	num,err := qs.All(&categorys)
+	num,err := qs.RelatedSel().All(&categorys)
+	//关联查询出相应分类下的所有文章信息
+	for _, v :=range categorys {
+		_, err = orm.NewOrm().LoadRelated(v, "Articles")
+	}
 	fmt.Printf("Returned Rows Num: %s, %s", num, err)
 	return categorys
 }
+
+//通过分类id获取分类信息
+func GetCategory(id int) (Category, error){
+	o := orm.NewOrm()
+	qs := o.QueryTable("category")
+	var cat Category
+	err := qs.Filter("Id",id).One(&cat)
+	return cat, err
+}
+
 
 //删除文章
 func DeleteArticle(id int) error  {
 	o := orm.NewOrm()
 	_, err := o.Delete(&Article{Id: id})
+	return err
+}
+//添加栏目
+func AddCategory(addCat Category)(int64 ,error){
+	o := orm.NewOrm()
+	o.Using("default")
+	category := new(Category)
+	category.Alias = addCat.Alias
+	category.Author = addCat.Author
+	category.Name = addCat.Name
+	category.Description = addCat.Description
+	category.Keywords = addCat.Keywords
+	id,err:=o.Insert(category)
+	return id,err
+}
+
+//更新栏目
+func UpdateCategory(updCat Category) error{
+	o := orm.NewOrm()
+	o.Using("default")
+	cat := Category{Id: updCat.Id}
+	cat.Keywords = updCat.Keywords
+	cat.Name = updCat.Name
+	cat.Description = updCat.Description
+	cat.Alias = updCat.Alias
+	cat.Author = updCat.Author
+	_, err := o.Update(&cat)
+	return err
+}
+
+func DeleteCategory(id int) error{
+	o := orm.NewOrm()
+	_, err := o.Delete(&Category{Id: id})
 	return err
 }
 
