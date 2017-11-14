6b4b97bffba6dab33373bcc4bb69620973b8f80a
TeriMoni <351111617@qq.com>
2017-11-14 18:03:54 +0800

    控制器

--- controllers/comment.go
+++ controllers/comment.go
@@ -4,6 +4,10 @@ import (
 	"time"
 
 	. "NewLife/models"
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/utils/pagination"
+	"strconv"
+	"fmt"
 )
 
 //添加评论
@@ -57,23 +61,100 @@ type EditCommentController struct {
 }
 
 func (this *EditCommentController) Post() {
+
 	if !this.isLogin {
-		this.Data["json"] = map[string]interface{}{"code": 0, "message": this.isLogin}
-		this.ServeJSON()
+		this.Redirect("/login", 302)
 		return
 	}
-	id, _ := this.GetInt("id")
-	status, _ := this.GetInt("status")
-
-	var com Comment
-	com.Status = status
-
-	err := UpdateComment(id, com)
+	comment := Comment{}
+	if err:=this.ParseForm(&comment);err !=nil{
+		fmt.Printf("表单数据提交错误")
+	}
+	err := UpdateComment(comment)
 
 	if err == nil {
-		this.Data["json"] = map[string]interface{}{"code": 1, "message": "状态更新成功", "id": id}
+		this.Data["json"] = map[string]interface{}{"code": 1, "message": "评论修改成功", "id": comment.Id}
 	} else {
-		this.Data["json"] = map[string]interface{}{"code": 0, "message": "状态修改失败"}
+		this.Data["json"] = map[string]interface{}{"code": 0, "message": "评论修改失败"}
+	}
+	this.ServeJSON()
+}
+
+func (this *EditCommentController) Get() {
+
+	if !this.isLogin {
+		this.Redirect("/login", 302)
+		return
+	}
+	idstr := this.Ctx.Input.Param(":id")
+	id, err := strconv.Atoi(idstr)
+	if err != nil {
+		this.Redirect("/404.html", 302)
+	}
+	com,err2 := GetComment(id)
+
+	if err2 != nil {
+		this.Redirect("/404.html", 302)
+	}
+
+	this.Data["com"]=com
+	this.Data["slider"] = "comment"
+	this.TplName="update-comment.html"
+}
+
+type ListCommentController struct {
+	BaseController
+}
+//获取评论列表
+func (this *ListCommentController) Get() {
+
+	if !this.isLogin {
+		this.Redirect("/login", 302)
+		return
+	}
+	//评论分页
+	page, err1 := this.GetInt("p")
+	if err1 != nil {
+		page = 1
+	}
+	offset, err2 := beego.AppConfig.Int("pageoffset")
+	if err2 != nil {
+		offset = 9
+	}
+	condCom := make(map[string]string)
+	condCom["article_id"] = ""
+	if !this.isLogin {
+		condCom["status"] = "1"
+	}
+	countComment := CountComment(condCom)
+	paginator := pagination.SetPaginator(this.Ctx, offset, countComment)
+	_, _, coms := ListComment(condCom, page, offset)
+	this.Data["paginator"] = paginator
+	this.Data["coms"] = coms
+	this.Data["slider"] = "comment"
+	this.TplName="comment.html"
+}
+
+type DeleteCommentController struct {
+	BaseController
+}
+
+//删除评论
+func (this *DeleteCommentController) Get(){
+	if !this.isLogin {
+		this.Redirect("/login", 302)
+		return
+	}
+	idstr := this.Ctx.Input.Param(":id")
+	id, err := strconv.Atoi(idstr)
+	if err != nil {
+		this.Redirect("/404.html", 302)
+	}
+	err2 := DeleteComment(id)
+	if err2 == nil{
+		this.Data["json"] = map[string]interface{}{"code": 1, "message": "删除评论成功"}
+	}else{
+		this.Data["json"] = map[string]interface{}{"code": 0, "message": "删除评论失败"}
 	}
 	this.ServeJSON()
 }
