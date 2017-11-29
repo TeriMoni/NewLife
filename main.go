b312479c06df9b2d221c9e42b6bef51e6b5172bd
TeriMoni <351111617@qq.com>
2017-11-29 17:19:41 +0800

    启动项

--- main.go
+++ main.go
@@ -3,9 +3,23 @@ package main
 import (
 	_ "NewLife/routers"
 
+	"html/template"
+	"net/http"
+
+	_ "NewLife/initial"
+
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	beego.ErrorHandler("404", page_not_found)
 	beego.Run()
+}
+
+func page_not_found(rw http.ResponseWriter, r *http.Request) {
+	t, _ := template.New("404.tpl").ParseFiles("views/404.tpl")
+
+	data := make(map[string]interface{})
+	data["content"] = "page not found"
+	t.Execute(rw, data)
 }
